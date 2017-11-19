d1a113c4dea9e655f1bbd7c0515b69cef1008048
cryptagoras <26805155+cryptagoras@users.noreply.github.com>
2017-11-19 15:35:57 +0200

    Ban False Satoshi's Vision nodes

--- ban.sh
+++ ban.sh
@@ -11,25 +11,25 @@ SNAPSHOT=`wget -qO- https://bitnodes.21.co/api/v1/snapshots/ | jq '.results[0] |
 # Download the list of currently connected nodes
 wget ${SNAPSHOT//\"} -O $NODES_FILE
 
-NODES=$(cat $NODES_FILE | jq -r -c '.nodes | to_entries | map([.key + .value[1]]) | .[]')
+NODES=$(cat $NODES_FILE | jq -r -c '.nodes | to_entries | map([.key + .value[1]]) | .[]' | sed -n 's/ *//gp')
 COUNT=0
 
 for NODE in ${NODES[@]}; do
   # Find all/most SegShit nodes
-  if [[ $NODE == *"Satoshi:1.1"* || $NODE == *"(2x"* ]]; then
+  if [[ $NODE == *"ABC:0.16"* ]]; then
     IP=$(echo $NODE | egrep -o '([0-9]{1,3}\.){3}[0-9]{1,3}:[0-9]{4,5}')
     NAME=$(echo $NODE | egrep -o '\/.*\/')
 
     # IP exists
     if [[ ! -z $IP ]]; then
       # Ban node
-      $(bitcoin-cli setban ${IP%:*} "add" ${BAN_TIME} 2&>/dev/null) && echo "Banned a new SegShit8x node: $IP" || echo "The following SegShit8x node has already been banned: $IP"
+      $(bitcoin-cli setban ${IP%:*} "add" ${BAN_TIME} 2&>/dev/null) && echo "Banned a new FalseSatoshisVision node: $IP" || echo "The following FalseSatoshisVision node has already been banned: $IP"
 
       ((COUNT++))
     fi
   fi
 done
 
-echo Found and banned $COUNT nodes in total.
+echo Found and banned $COUNT FalseSatoshisVision nodes in total.
 
 rm $NODES_FILE
